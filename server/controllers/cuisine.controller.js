1de8bdc3e3059374490a4c2f6667a116596b9fd3
Oki <biek.syaoki@gmail.com>
2026-07-10 19:47:46 +0700

    feat: add cloudinary config environment and create new cuisine controller

--- server/controllers/cuisine.controller.js
+++ server/controllers/cuisine.controller.js
@@ -1,4 +1,5 @@
 const { Cuisine } = require("../models/");
+const { uploadToCloudinary } = require("../utils/cloudinary.util");
 
 class CuisineController {
   static async getCuisines(req, res, next) {
@@ -29,10 +30,46 @@ class CuisineController {
 
   static async postCuisine(req, res, next) {
     try {
-      // TODO: Create new cuisine
       const { originalname, buffer } = req.file;
-      res.status(200).json({ originalname, buffer });
+      const { name, description, price, categoryId } = req.body;
+      const authorId = req.user.id;
+
+      if (!name)
+        throw { name: "InvalidRequest", message: "nama tidak boleh kosong" };
+      if (!description)
+        throw {
+          name: "InvalidRequest",
+          message: "deskripsi tidak boleh kosong",
+        };
+      if (!price)
+        throw { name: "InvalidRequest", message: "harga tidak boleh kosong" };
+      if (!categoryId)
+        throw {
+          name: "InvalidRequest",
+          message: "kategori tidak boleh kosong",
+        };
+
+      if (!req.file)
+        throw { name: "InvalidRequest", message: "image tidak boleh kosong" };
+
+      const image = await uploadToCloudinary(buffer, originalname);
+
+      const cuisine = await Cuisine.create({
+        name,
+        description,
+        price,
+        imageUrl: image.secure_url,
+        imagePublicId: image.public_id,
+        categoryId,
+        authorId,
+      });
+
+      res.status(200).json({
+        message: `Successfully create new cuisine`,
+        object: cuisine,
+      });
     } catch (err) {
+      console.log(err);
       next(err);
     }
   }
